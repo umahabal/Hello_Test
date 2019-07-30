8644597c6bbfa90d1ba35b92882ea49521bba65d
umahabal <42170849+umahabal@users.noreply.github.com>
2019-07-30 13:04:51 +0530

    Project Upload

---
+++ hello_world_TEST/hello_world/hello_world/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello_world
+//
+//  Created by Unmesh on 8/23/18.
+//  Copyright © 2018 LTIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hello_world_TEST/hello_world/hello_world/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hello_world
+//
+//  Created by Unmesh on 8/23/18.
+//  Copyright © 2018 LTIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
